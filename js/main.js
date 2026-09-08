2e64d99302db23633ea19dc398aa6aee4b960a94
Minette Zongo M. <minettemeyo@gmail.com>
2026-09-08 14:44:03 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+
+const navLinks = document.querySelectorAll(".nav-links a");
+const sections = document.querySelectorAll(".content-section");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+
+        const target = link.getAttribute("href").substring(1);
+
+        sections.forEach(section => {
+            section.style.display =
+                section.id === target ? "block" : "none";
+        });
+    });
+});
+```
 
